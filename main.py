10e1406bc88f0a642204ad049ef3c1a9ad1e872f
kuchm <bogdanovivvan670@gmail.com>
2022-04-01 12:22:47 +0300

    кик и мут/тест

--- main.py
+++ main.py
@@ -28,10 +28,34 @@ class RandomThings(commands.Cog):
         num = random.randint(int(min_int), int(max_int))
         await ctx.send(num)
 
+    @commands.command(name='mute')
+    @commands.has_any_role(959376510509269032)
+    async def mute(self, ctx, member: discord.Member, time: int):
+        emb = discord.Embed(title="Участник Был Замучен!", colour=discord.Color.blue())
+        await ctx.channel.purge(limit=1)
+
+        emb.set_author(name=member.name, icon_url=member.avatar_url)
+        emb.set_footer(text="Его замутил {}".format(ctx.author.name), icon_url=ctx.author.avatar_url)
+
+        await ctx.send(embed=emb)
+        muted_role = discord.utils.get(ctx.message.guild.roles, name="Muted")
+        await member.add_roles(muted_role)
+
+        await asyncio.sleep(time)
+        await member.remove_roles(muted_role)
+
+    @commands.command(name='kick')
+    async def kick(self, ctx, member: discord.Member, *, reason=None, amount=1):
+        await ctx.channel.purge(limit=int(amount))
+        await member.kcik(reason=reason)
+        await ctx.send(f'kick user {member.mention}')
+
+
+
 
 bot = commands.Bot(command_prefix='//', intents=intents)
 bot.add_cog(RandomThings(bot))
 
-TOKEN = "OTU4NjI5NjkxNjk3Mjc5MDA2.YkQHeA.yYrUUZj92IZVYs-_YQeRiPAsG8o"
+TOKEN = "***"
 
 bot.run(TOKEN)
