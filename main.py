37ad9976a16331f4d77946b46bf2c2b4163802a2
kuchm <bogdanovivvan670@gmail.com>
2022-04-02 18:12:04 +0300

    рабочий мут и кик

--- main.py
+++ main.py
@@ -18,44 +18,53 @@ class RandomThings(commands.Cog):
         self.bot = bot
 
     @commands.command(name='ban')
+    @commands.has_permissions(administrator=True)
     async def ban(self, ctx, member: discord.Member, *, reason=None, amount=1):
         await ctx.channel.purge(limit=int(amount))
         await member.ban(reason=reason)
-        await ctx.send(f'ban user {member.mention}')
+        await ctx.send(f'О мой бог этот парень реально пререшел черту { member.mention }')
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def unban(self, ctx, *, member):
+        await ctx.channel.purge(limit=1)
+        banned_users = await ctx.guild.bans()
+        for ban_entry in banned_users:
+            user = ban_entry.user
+            await ctx.guild.unban(user)
+            await ctx.send(f'{ctx.author.name} сегодня добрый , он простил {user.mention}')
+            return
 
     @commands.command(name='roll')
     async def roll(self, ctx, min_int, max_int):
         num = random.randint(int(min_int), int(max_int))
         await ctx.send(num)
 
-    @commands.command(name='mute')
-    @commands.has_any_role(959376510509269032)
-    async def mute(self, ctx, member: discord.Member, time: int):
-        emb = discord.Embed(title="Участник Был Замучен!", colour=discord.Color.blue())
+    @commands.command()
+    async def mute(self, ctx, member: discord.Member, *, reason=None):
+        mute = discord.utils.get(ctx.guild.roles, name='лох в муте')
+        await member.move_to(channel=None)
+        await member.add_roles(mute)
+        await ctx.send(f'{ctx.author.name} замутил(а) {member.mention} по причине {reason}.')
+
+    @commands.command()
+    async def unmute(self, ctx, member: discord.Member):
+        mute = discord.utils.get(ctx.guild.roles, name='лох в муте')
+        await member.remove_roles(mute)
+        await ctx.send(f'{ctx.author.name} раззамутил(а) {member.mention}.')
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def kick(self, ctx, member: discord.Member, *, reason='Гнев Бога'):
         await ctx.channel.purge(limit=1)
 
-        emb.set_author(name=member.name, icon_url=member.avatar_url)
-        emb.set_footer(text="Его замутил {}".format(ctx.author.name), icon_url=ctx.author.avatar_url)
-
-        await ctx.send(embed=emb)
-        muted_role = discord.utils.get(ctx.message.guild.roles, name="Muted")
-        await member.add_roles(muted_role)
-
-        await asyncio.sleep(time)
-        await member.remove_roles(muted_role)
-
-    @commands.command(name='kick')
-    async def kick(self, ctx, member: discord.Member, *, reason=None, amount=1):
-        await ctx.channel.purge(limit=int(amount))
-        await member.kcik(reason=reason)
-        await ctx.send(f'kick user {member.mention}')
-
-
+        await member.kick(reason=reason)
+        await ctx.send(f'{ctx.author.name} в гневе и выкинул с олимпа {member.mention}')
 
 
 bot = commands.Bot(command_prefix='//', intents=intents)
 bot.add_cog(RandomThings(bot))
 
-TOKEN = "***"
+TOKEN = "OTU4NjI5NjkxNjk3Mjc5MDA2.YkQHeA.Q7k6cGWJIYbMr_ypA5cAq4ZrRk4"
 
 bot.run(TOKEN)
